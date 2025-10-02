24f75038094f948ab0aac3925c5f62c11da72411
s0k0a9l <sowmya.kasiramasastry@walmart.com>
2025-10-02 15:46:15 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/powerstemsolutions/', // Set this to your repo name for GitHub Pages
+    base: '/', // Use root for custom domain
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
